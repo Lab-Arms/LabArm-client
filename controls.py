81644b21ba07a07cd33dc4e1d636b150a2102310
Luciano Almeida <mdiebr@gmail.com>
2016-06-01 13:27:18 -0300

    Button click fill and fonts
    Signed-off-by: Luciano Almeida <mdiebr@gmail.com>

--- controls.py
+++ controls.py
@@ -9,57 +9,57 @@ def button_clicked(pos):
         x > MOTOR1HORARIO_X and x < MOTOR1HORARIO_X + BUTTON_WIDTH and
         y > MOTOR1HORARIO_Y and y < MOTOR1HORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M1H"
+        return "0"
     elif (
         x > MOTOR1ANTIHORARIO_X and x < MOTOR1ANTIHORARIO_X + BUTTON_WIDTH and
         y > MOTOR1ANTIHORARIO_Y and y < MOTOR1ANTIHORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M1A"
+        return "1"
 
     # Motor 2 buttons
     elif (
         x > MOTOR2HORARIO_X and x < MOTOR2HORARIO_X + BUTTON_WIDTH and
         y > MOTOR2HORARIO_Y and y < MOTOR2HORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M2H"
+        return "2"
     elif (
         x > MOTOR2ANTIHORARIO_X and x < MOTOR2ANTIHORARIO_X + BUTTON_WIDTH and
         y > MOTOR2ANTIHORARIO_Y and y < MOTOR2ANTIHORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M2A"
+        return "3"
 
     # Motor 3 buttons
     elif (
         x > MOTOR3HORARIO_X and x < MOTOR3HORARIO_X + BUTTON_WIDTH and
         y > MOTOR3HORARIO_Y and y < MOTOR3HORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M3H"
+        return "4"
     elif (
         x > MOTOR3ANTIHORARIO_X and x < MOTOR3ANTIHORARIO_X + BUTTON_WIDTH and
         y > MOTOR3ANTIHORARIO_Y and y < MOTOR3ANTIHORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M3A"
+        return "5"
 
     # Motor 4 buttons
     elif (
         x > MOTOR4HORARIO_X and x < MOTOR4HORARIO_X + BUTTON_WIDTH and
         y > MOTOR4HORARIO_Y and y < MOTOR4HORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M4H"
+        return "6"
     elif (
         x > MOTOR4ANTIHORARIO_X and x < MOTOR4ANTIHORARIO_X + BUTTON_WIDTH and
         y > MOTOR4ANTIHORARIO_Y and y < MOTOR4ANTIHORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M4A"
+        return "7"
 
     # Motor 5 buttons
     elif (
         x > MOTOR5HORARIO_X and x < MOTOR5HORARIO_X + BUTTON_WIDTH and
         y > MOTOR5HORARIO_Y and y < MOTOR5HORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M5H"
+        return "8"
     elif (
         x > MOTOR5ANTIHORARIO_X and x < MOTOR5ANTIHORARIO_X + BUTTON_WIDTH and
         y > MOTOR5ANTIHORARIO_Y and y < MOTOR5ANTIHORARIO_Y + BUTTON_HEIGHT
     ):
-        return "M5A"
+        return "9"
--- main.py
+++ main.py
@@ -2,26 +2,46 @@
 
 import sys
 import pygame
+import pygame.camera
 from pygame.locals import *
 from images import *
-from constants import *
 from controls import *
 
 pygame.init()
+clock = pygame.time.Clock()
+# pygame.camera.init()
+# camm = pygame.camera.Camera("/dev/video0", (640, 480))
+# camm.start()
+# img = camm.get_image()
+# screen.blit(img, cam)
+# camm.stop()
 
-size = width, height = 1200, 700
+size = width, height = 1200, 715
 white = 254, 254, 254
+gray = 200, 200, 200
+black = 0, 0, 0
+
 clicked = None
 
 screen = pygame.display.set_mode(size)
+cam_rect = pygame.Rect(450, 250, 732, 450)
+font = pygame.font.SysFont("timesnewroman", 54)
+text1 = font.render("Desconectado do servidor.", True, black)
+text2 = font.render("Deseja conectar?", True, black)
 
 
 def draw():
     screen.fill(white)
-    if not clicked:
-        for k, v in imagens.items():
-            i, r = v
-            screen.blit(i, r)
+    if clicked:
+        _, rectangle = imagens[int(clicked) + 1]
+        screen.fill(gray, rectangle)
+    for k, v in imagens.items():
+        surface, rectangle = v
+        screen.blit(surface, rectangle)
+
+    pygame.draw.rect(screen, black, cam_rect, 3)
+    screen.blit(text1, (cam_rect.centerx - text1.get_width() / 2, cam_rect.centery-25 - text1.get_height() / 2, ))
+    screen.blit(text2, (cam_rect.centerx - text2.get_width() / 2, cam_rect.centery+25 - text2.get_height() / 2, ))
     pygame.display.flip()
 
 
