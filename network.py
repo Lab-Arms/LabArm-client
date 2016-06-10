8d40002040e5c39d9003a745c530cec5c85b55b0
Luciano Almeida <mdiebr@gmail.com>
2016-06-10 02:09:23 -0300

    inicia refatoração
    Signed-off-by: Luciano Almeida <mdiebr@gmail.com>

---
+++ network.py
@@ -0,0 +1,16 @@
+import socket
+
+
+class Network():
+    address = host, port = '127.0.0.1', 4004
+
+    def connect_to_server(self):
+        try:
+            sock = socket.create_connection(self.address, timeout=10)
+            sock.settimeout(None)
+        except ConnectionRefusedError:
+            sock = -1
+        return sock
+
+    def disconnect_from_server(self):
+        pass
